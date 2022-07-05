259b491b1dc785d28efa03fff5bec235abbf21bb
osho81 <ossi.hour@gmail.com>
2022-07-05 22:33:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Basic Bank App 
+
+### Description
+Basic bank program enabling account and customer registration, payment transactions etc. 
+
+### Motivation & purpose
+Preparations for coming full-stack course within the ongoing system developer education.
+
+### Techs used
+- Java
+- Spring boot
+- Main Spring modules: spring web, data jpa, MySql driver. 
+- MySql & MySql-Workbench
+
---
+++ src/main/java/com/yaelev/bank/BankApplication.java
@@ -0,0 +1,21 @@
+package com.yaelev.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // rest API component: https://spring.io/guides/tutorials/rest/
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello Spring";
+	}
+
+}
