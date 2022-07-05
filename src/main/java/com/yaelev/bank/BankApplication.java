427585e13f566bc61e81ddf4bfbf56df43e135d3
osho81 <ossi.hour@gmail.com>
2022-07-05 23:11:58 +0200

    Initial commit

--- src/main/java/com/yaelev/bank/BankApplication.java
+++ src/main/java/com/yaelev/bank/BankApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController // rest API component: https://spring.io/guides/tutorials/rest/
 public class BankApplication {
@@ -13,9 +15,9 @@ public class BankApplication {
 		SpringApplication.run(BankApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello() {
-		return "Hello Spring";
+	@GetMapping // Type of Rest endpoint: https://docs.spring.io/spring-boot/docs/2.1.1.RELEASE/reference/html/production-ready-endpoints.html
+	public List<String> hello() {
+		return List.of("Hello", "Spring");
 	}
 
 }
