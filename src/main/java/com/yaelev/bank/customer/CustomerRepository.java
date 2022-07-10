ffcfb7d7fd9aa907dfce80cda6d5b417f5b7d337
osho81 <ossi.hour@gmail.com>
2022-07-10 14:44:40 +0200

    Update CustomerRepository

--- src/main/java/com/yaelev/bank/customer/CustomerRepository.java
+++ src/main/java/com/yaelev/bank/customer/CustomerRepository.java
@@ -2,13 +2,14 @@ package com.yaelev.bank.customer;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 // "Data access layer"; enables CRUD operations; used by related service class
 
 // Specify class to service with CRUD methods etc., and specify Id type
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-    // Customizedd operations and queries
-
-
+    // Customized operations and queries
+    Optional<Customer> findCustomerByEmail(String email); // Container
 }
 
