fb92d529e2ff00f9b28085914cdf66af0c4bfa8c
osho81 <ossi.hour@gmail.com>
2022-07-11 23:19:02 +0200

    Correct to standard endpoints

--- src/main/java/com/yaelev/bank/customer/CustomerController.java
+++ src/main/java/com/yaelev/bank/customer/CustomerController.java
@@ -9,7 +9,7 @@ import java.util.List;
 // (while service have more specific methods such as CRUD operations)
 
 @RestController // Rest API component: https://spring.io/guides/tutorials/rest/
-@RequestMapping(path = "/api/v1") // Either use this main routing for whole class or specify at endpoint methods
+@RequestMapping(path = "/api/v1") // Use this main routing for whole class or specify at GET endpoint
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -25,7 +25,7 @@ public class CustomerController {
     // Major types of Rest endpoints;
     // https://docs.spring.io/spring-boot/docs/2.1.1.RELEASE/reference/html/production-ready-endpoints.html
 
-    @GetMapping //("customers") // Either use these endpoint routings or main RequestMapping above
+    @GetMapping // ("customers") // Use this endpoint api routing or in @RequestMapping above
     public List<Customer> customers() {
         return customerService.getCustomers();
     }
