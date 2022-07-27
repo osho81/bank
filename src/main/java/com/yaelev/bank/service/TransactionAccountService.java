602a6330ccf0c56aa37bfb936dc6f9ad1ff8d80e
osho81 <ossi.hour@gmail.com>
2022-07-27 23:23:30 +0200

    Add transactionaccount repository Interface

--- src/main/java/com/yaelev/bank/service/TransactionAccountService.java
+++ src/main/java/com/yaelev/bank/service/TransactionAccountService.java
@@ -1,10 +1,22 @@
 package com.yaelev.bank.service;
 
 import com.yaelev.bank.model.TransactionAccount;
-
+import com.yaelev.bank.repository.TransactionAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class TransactionAccountService {
+
+    private final TransactionAccountRepository transactionAccountRepository;
+
+    @Autowired
+    public TransactionAccountService(TransactionAccountRepository transactionAccountRepository) {
+        this.transactionAccountRepository = transactionAccountRepository;
+    }
+
     public List<TransactionAccount> getCustomers() {
+       return (List<TransactionAccount>) transactionAccountRepository.findAll();
     }
 }
