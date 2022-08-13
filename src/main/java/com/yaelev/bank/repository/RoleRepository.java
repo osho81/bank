3a85a0f620b5d35f953939bd9a30f0d75f3b4322
osho81 <ossi.hour@gmail.com>
2022-08-13 15:31:19 +0200

    Start initial security setup

--- src/main/java/com/yaelev/bank/repository/RoleRepository.java
+++ src/main/java/com/yaelev/bank/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.yaelev.bank.repository;
 
-public interface RoleRepository {
+import com.yaelev.bank.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findRoleByName(String name);
 }
