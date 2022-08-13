3affe999c589779754dc4fa09c8c518af5c98c05
osho81 <ossi.hour@gmail.com>
2022-08-13 15:33:31 +0200

    Start initial security setup

--- src/main/java/com/yaelev/bank/repository/AppUserRepository.java
+++ src/main/java/com/yaelev/bank/repository/AppUserRepository.java
@@ -4,6 +4,7 @@ import com.yaelev.bank.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+// Spring Security component
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findAppUserByUsername(String username);
     // OR: User findAppUserByUsername(String username);
--- src/main/java/com/yaelev/bank/repository/RoleRepository.java
+++ src/main/java/com/yaelev/bank/repository/RoleRepository.java
@@ -2,9 +2,9 @@ package com.yaelev.bank.repository;
 
 import com.yaelev.bank.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
+// Spring Security component'
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findRoleByName(String name);
 }
