992c63be6cd1fa453c4984893728f20f2393605c
osho81 <ossi.hour@gmail.com>
2022-08-13 15:28:22 +0200

    Start initial security setup

--- src/main/java/com/yaelev/bank/repository/AppUserRepository.java
+++ src/main/java/com/yaelev/bank/repository/AppUserRepository.java
@@ -2,6 +2,9 @@ package com.yaelev.bank.repository;
 
 import com.yaelev.bank.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findAppUserByUsername(String username);
+    // OR: User findAppUserByUsername(String username);
 }
