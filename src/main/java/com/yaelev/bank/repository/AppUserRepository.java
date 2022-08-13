dd7bad3729f449f42b82d74090834cf74af7728a
osho81 <ossi.hour@gmail.com>
2022-08-13 22:46:49 +0200

    Update spring security service configuration

--- src/main/java/com/yaelev/bank/repository/AppUserRepository.java
+++ src/main/java/com/yaelev/bank/repository/AppUserRepository.java
@@ -2,10 +2,8 @@ package com.yaelev.bank.repository;
 
 import com.yaelev.bank.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 // Spring Security component
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findAppUserByUsername(String username);
-    // OR: User findAppUserByUsername(String username);
 }
