b94cf2599cd7888cf9a2657e604f906afff4e88f
osho81 <ossi.hour@gmail.com>
2022-08-16 10:57:10 +0200

    Basic spring security setup

--- src/main/java/com/yaelev/bank/service/SecurityConfig.java
+++ src/main/java/com/yaelev/bank/service/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.yaelev.bank.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    @Bean // Customize login, authentication procedures etc
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
@@ -22,8 +23,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
 //                .antMatchers(HttpMethod.GET, "/t-account/**").permitAll() // Anyone can GET
 //                .antMatchers(HttpMethod.POST, "/etc...").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ROLE_ADMIN")
 //                .antMatchers(HttpMethod.DELETE, "/someOtherPath/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
+                .and()
+                .logout().invalidateHttpSession(true)
                 .and()
                 .csrf().disable();
         return http.build();
