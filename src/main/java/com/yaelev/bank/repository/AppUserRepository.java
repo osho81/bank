aa20a8f2d98b717ecdead71dbfd1413c52c806a9
osho81 <ossi.hour@gmail.com>
2022-09-03 22:13:08 +0200

    Connect Customer table to AppUser table via userEmail

--- src/main/java/com/yaelev/bank/repository/AppUserRepository.java
+++ src/main/java/com/yaelev/bank/repository/AppUserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // Spring Security component
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    // AppUser findAppUserByUsername(String username);
+    // AppUser findAppUserByUsername(String username); // Reserve method during development
     AppUser findAppUserByEmail(String email);
 }
