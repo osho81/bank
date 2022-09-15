c2657453b2984e30ad04c6a4983a35150a842bad
osho81 <ossi.hour@gmail.com>
2022-09-15 14:15:09 +0200

    Connect Customer table to AppUser table via userEmail

--- src/main/java/com/yaelev/bank/BankApplication.java
+++ src/main/java/com/yaelev/bank/BankApplication.java
@@ -2,12 +2,9 @@ package com.yaelev.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 // Bank app; backend component
-// Related frontend with react/js, see bankfrontend in github
+// Related frontend with react/js, see bankfrontend in Github
 
 @SpringBootApplication
 public class BankApplication {
